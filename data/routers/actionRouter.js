5725b854121319dd8e3df8a34eb0203eae138a23
schang1146 <sammychang2185@gmail.com>
2019-08-04 20:34:34 -0700

    action CRUD operations functioning

--- data/routers/actionRouter.js
+++ data/routers/actionRouter.js
@@ -13,11 +13,27 @@ router.get("/:id", validateId, (req, res) => {
     res.status(200).json(req.action);
 });
 
-router.post("/", (req, res) => {});
+router.post("/", validateAction, (req, res) => {
+    Action.insert(req.actionInfo)
+        .then(res.status(201).json(req.actionInfo))
+        .catch(error => res.status(500).json(error));
+});
 
-router.put("/:id", (req, res) => {});
+router.put("/:id", validateId, validateAction, (req, res) => {
+    Action.update(req.actionId, req.actionInfo)
+        .then(res.status(202).json(req.actionInfo))
+        .catch(error => res.status(500).json(error));
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", validateId, (req, res) => {
+    Action.remove(req.actionId)
+        .then(
+            res
+                .status(204)
+                .json({ message: `removed action id: ${req.actionId}` })
+        )
+        .catch(error => res.status(500).json(error));
+});
 
 // custom middleware
 async function validateId(req, res, next) {
@@ -27,10 +43,33 @@ async function validateId(req, res, next) {
         const action = await Action.get(id);
 
         if (action) {
+            req.actionId = id;
             req.action = action;
             next();
         } else {
             res.status(400).json({ error: "invalid action id" });
+        }
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+async function validateAction(req, res, next) {
+    try {
+        const actionInfo = req.body;
+
+        if (
+            actionInfo.project_id &&
+            actionInfo.description &&
+            actionInfo.notes
+        ) {
+            req.actionInfo = actionInfo;
+            next();
+        } else {
+            res.status(400).json({
+                error:
+                    "please make sure project_id, description, and notes are sent"
+            });
         }
     } catch (error) {
         res.status(500).json(error);
